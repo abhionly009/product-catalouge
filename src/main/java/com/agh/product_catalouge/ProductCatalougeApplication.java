5ca4465b89ab07dcc42072caecf6960dd3231467
abhi <50333572+abhionly009@users.noreply.github.com>
2025-08-26 16:17:06 +0530

    add code to log failure log to kafka and custom exception when invalid credentials

--- src/main/java/com/agh/product_catalouge/ProductCatalougeApplication.java
+++ src/main/java/com/agh/product_catalouge/ProductCatalougeApplication.java
@@ -2,7 +2,9 @@ package com.agh.product_catalouge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductCatalougeApplication {
 
