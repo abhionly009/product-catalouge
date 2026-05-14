4d49c17ae35f728dd9b781fe2f01a73177fe6092
abhi <50333572+abhionly009@users.noreply.github.com>
2026-05-14 10:46:53 +0530

    add code to validate request so that it will prevent invalid requests going to business logic

--- src/main/java/com/agh/product_catalouge/controller/ProductController.java
+++ src/main/java/com/agh/product_catalouge/controller/ProductController.java
@@ -3,14 +3,17 @@ package com.agh.product_catalouge.controller;
 import com.agh.product_catalouge.entity.Product;
 import com.agh.product_catalouge.entity.ProductDTOV1;
 import com.agh.product_catalouge.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Validated
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
@@ -24,29 +27,28 @@ public class ProductController {
 
 
     @PostMapping("/add")
-    public ResponseEntity<Product> createProduct(@RequestBody ProductDTOV1 product){
+    public ResponseEntity<Product> createProduct( @Valid @RequestBody ProductDTOV1 product){
         Product savedProduct = productService.addProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
-
     }
 
 
     @GetMapping("/getAll")
     public ResponseEntity<List<Product>> getAllProduct(){
-       return ResponseEntity.status(HttpStatus.OK).body( productService.getAllProducts());
+        return ResponseEntity.status(HttpStatus.OK).body( productService.getAllProducts());
     }
 
 
 
     @PostMapping("/addProducts")
     public ResponseEntity<List<Product>> addProducts(@RequestBody List<Product> products){
-       return ResponseEntity.status(HttpStatus.CREATED).body( productService.addProductInBulk(products));
+        return ResponseEntity.status(HttpStatus.CREATED).body( productService.addProductInBulk(products));
     }
 
     @GetMapping("/productDetails")
     public ResponseEntity<Optional<Product>> getProductDetails(@RequestParam long id ){
 
-       return ResponseEntity.status(HttpStatus.OK).body(productService.getProductDetails(id));
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProductDetails(id));
 
     }
 
@@ -56,8 +58,8 @@ public class ProductController {
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(defaultValue = "asc") String direction
-            ){
-       return ResponseEntity.status(HttpStatus.OK).body(productService.getProductsWithPage(page,size,sortBy,direction));
+    ){
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProductsWithPage(page,size,sortBy,direction));
 
     }
 }
