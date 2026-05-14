9f9a170acb217dd64ca7869df2c6264054291aaa
abhi <50333572+abhionly009@users.noreply.github.com>
2026-05-14 10:48:10 +0530

    remove validated from controller which is not required at the moment

--- src/main/java/com/agh/product_catalouge/controller/ProductController.java
+++ src/main/java/com/agh/product_catalouge/controller/ProductController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Validated
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
